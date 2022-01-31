3e1c73d3f580f99465e64cc74a8eb73d5fe7fc61
edsown <edsonoliveira.contato@gmail.com>
2022-01-31 14:38:47 -0300

    navbar layout complete

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,19 @@
 import React from "react";
-
+import { GiFlamer } from "react-icons/gi";
 function Navbar() {
   return (
-    <div>
-      <div className="top-0 fixed w-full bg-slate-600 flex justify-items-start h-16">
-        <div className="logo text-violet-50">a</div>
+    <>
+      <div className="navbar top-0 fixed w-full bg-slate-600 flex h-16 p-4 px-4 lg:px-64 justify-center lg:text-2xl text-xl">
+        <div className="logo lg:text-4xl text-3xl text-violet-50 flex flex-row mr-auto">
+          <GiFlamer />
+          <p className=" text-2xl ml-2 hidden lg:block">CHAMA SOLAR</p>
+        </div>
+        <ul className="flex lg:space-x-12 space-x-4 text-violet-50 ">
+          <li className="menu-item">Entenda</li>
+          <li className="menu-item">Orçamento</li>
+        </ul>
       </div>
-    </div>
+    </>
   );
 }
 
