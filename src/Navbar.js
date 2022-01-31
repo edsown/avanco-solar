6266a38c7d5e2b6604c46b518329955b108c0c0b
edsown <edsonoliveira.contato@gmail.com>
2022-01-31 13:35:06 -0300

    testing tailwind features

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <div className="top-0 fixed w-full bg-slate-600 flex justify-items-start h-16">
+        <div className="logo text-violet-50">a</div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
